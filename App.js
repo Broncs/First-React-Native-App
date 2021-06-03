27811dce75c8225aac61f59e3de4dba9f9cf756e
Jhonatan mendes <jhowfrombrazil@gmail.com>
2021-06-03 19:00:37 -0300

    add flat list component

--- App.js
+++ App.js
@@ -1,38 +1,74 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
+import {
+  Button,
+  FlatList,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+} from "react-native";
 
 export default function App() {
+  const [enteredGoal, setEnteredGoal] = useState("");
+  const [goals, setGoals] = useState([]);
+
+  const goalInputHandler = (enteredText) => {
+    setEnteredGoal(enteredText);
+  };
+
+  const addGoalHandler = () => {
+    setGoals((currentGoals) => [...currentGoals, {key: Math.random().toString(), value: enteredGoal}]);
+    setEnteredGoal("");
+  };
+
   return (
-    <View style={styles.app}>
-      <View style={styles.goal}>
-          <TextInput style={styles.input} placeholder="Type your goal"/>
-          <Button title="ADD"/>
+    <View style={styles.screen}>
+      <View style={styles.inputContainer}>
+        <TextInput
+          style={styles.input}
+          placeholder="Type your goal"
+          value={enteredGoal}
+          onChangeText={goalInputHandler}
+        />
+        <Button onPress={addGoalHandler} title="ADD" />
       </View>
-      <View>
-        
-      </View>
-      
+      <FlatList
+        data={goals}
+        renderItem={(itemData) => (
+          <View style={styles.listItem}>
+            <Text>{itemData.item.value}</Text>
+          </View>
+        )}
+      />
+
       <StatusBar style="auto" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
- app:{
-   padding: 30,
- },
- input:{
-   borderColor: 'black',
-   borderWidth: 1,
-   padding: 10,
-   marginBottom: 5,
-   width: '80%'
- },
- goal:{
-  flexDirection: "row",
-  justifyContent: 'space-between',
-  alignItems: 'center',
- }
- 
+  screen: {
+    padding: 30,
+  },
+  inputContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  input: {
+    borderColor: "black",
+    borderWidth: 1,
+    padding: 10,
+    marginBottom: 5,
+    width: "80%",
+  },
+  listItem: {
+    padding: 10,
+    marginVertical: 10,
+    backgroundColor: "#ccc",
+    borderColor: "black",
+    borderWidth: 1,
+  },
 });
